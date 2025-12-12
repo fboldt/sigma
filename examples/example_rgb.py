6bd1990c5ea6f11e12e1c9e4fc0beee6f531e1e2
Izabelly Glassiner <izabelly.cristine.ic@gmail.com>
2025-12-12 10:20:44 -0300

    Ajustes na função de composição RGB

--- examples/example_rgb.py
+++ examples/example_rgb.py
@@ -5,8 +5,11 @@ def example_rgb():
     # 1. Diretório contendo as bandas individuais
     input_path = './images/bandas' 
 
-    # 2. Diretório de saída para a imagem composta
-    output_path = './images' 
+    # 2. Diretório e nome do arquivo de saída para a imagem composta
+    output_dir = './images' 
+    output_filename = 'composicao_rgb.tif'
+
+    output_file_path = os.path.join(output_dir, output_filename)
 
     # Caminhos completos para as bandas vermelha, verde e azul
     red_band_path = os.path.join(input_path, 'BAND3.tif')
@@ -14,7 +17,7 @@ def example_rgb():
     blue_band_path = os.path.join(input_path, 'BAND1.tif')
 
     # 3. Chamada da função para compor a imagem RGB
-    rgb_composite(red_band_path, green_band_path, blue_band_path, output_path)
+    rgb_composite(red_band_path, green_band_path, blue_band_path, output_file_path)
 
 if __name__ == "__main__":
     example_rgb()
--- utils/rgb.py
+++ utils/rgb.py
@@ -1,17 +1,16 @@
+import os
 from cbers4asat.tools import rgbn_composite
 import rasterio as rio
 
-def rgb_composite(red_band, green_band, blue_band, output_dir):
+def rgb_composite(red_band, green_band, blue_band, output_file_path):
 
-    output_filename = 'CBERS4A_TRUE_COLOR.tif' # Nome do arquivo de saída
-    output_path = f'{output_dir}/{output_filename}' # Caminho completo do arquivo de saída
-
+    #1. Definir o diretório e o nome do arquivo de saída
+    output_dir = os.path.dirname(output_file_path)
+    output_filename = os.path.basename(output_file_path)
+    
     # Criando a composição cor verdadeira
     rgbn_composite(red=red_band,
                 green=green_band,
                 blue=blue_band,
                 filename=output_filename,
                 outdir=output_dir)
-    
-    # Mensagem de confirmação
-    print(f'Composição RGB salva em: {output_path}')
