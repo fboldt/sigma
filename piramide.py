250e3aeced5d3a51b8bc19222633e6906ce80bdb
julia-almeida09 <juliaalmeidanasc8@gmail.com>
2026-04-09 10:41:38 -0300

    Teste para cores

--- piramide.py
+++ piramide.py
@@ -6,7 +6,7 @@ import shutil # Biblioteca para copiar arquivos
 def gerar_copia_com_piramides(caminho_original, fatores=[2, 4, 8, 16, 32]):
     # 1. Criar o nome do novo arquivo (ex: imagem_com_py.tif)
     nome_base, extensao = os.path.splitext(caminho_original)
-    caminho_novo = f"{nome_base}_com_piramides{extensao}"
+    caminho_novo = f"{nome_base}_com_piramides{extensao}2"
 
     # 2. Verificar se o arquivo original existe
     if not os.path.exists(caminho_original):
@@ -21,7 +21,7 @@ def gerar_copia_com_piramides(caminho_original, fatores=[2, 4, 8, 16, 32]):
 
         # 4. Abrir a CÓPIA para gerar as pirâmides
         with rasterio.open(caminho_novo, mode='r+') as dst:
-            dst.build_overviews(fatores, Resampling.average)
+            dst.build_overviews(fatores, Resampling.nearest)
             dst.update_tags(ns='rio_utils', overviews=str(fatores))
             
             print(f"Sucesso! Pirâmides adicionadas à cópia.")
@@ -31,5 +31,5 @@ def gerar_copia_com_piramides(caminho_original, fatores=[2, 4, 8, 16, 32]):
         print(f"Ocorreu um erro: {e}")
 
 # --- Exemplo de uso ---
-arquivo_antigo = "ifes_resultado_pansharpening_raiz01.tif"
+arquivo_antigo = ".\images\MOSAIC_NOVO.tif"
 novo_arquivo = gerar_copia_com_piramides(arquivo_antigo)
