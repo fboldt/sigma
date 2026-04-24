36fd6eaa6ba559e6add61fab3d2f31adc00dd4ee
julia-almeida09 <juliaalmeidanasc8@gmail.com>
2026-04-24 15:20:22 -0300

    correção do examplo de piramide

--- examples/example_piramide.py
+++ examples/example_piramide.py
@@ -1,9 +1,9 @@
 import os
-from utils import piramide
+from utils.piramide import gerar_copia_com_piramides
 
 def example_piramide():
     # Caminho para a imagem original (sem pirâmides)
-    caminho_imagem_original = './images/MOSAIC_NOVO.tif' 
+    caminho_imagem_original = './images/MOSAIC_NOVO' 
 
     # Gerar a cópia com pirâmides
     caminho_imagem_com_piramides = gerar_copia_com_piramides(caminho_imagem_original) #pode adicionar os fatores de pirâmides como segundo argumento, ex: [2, 4, 8]
