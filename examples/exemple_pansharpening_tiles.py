3b210fc424d5808359f11eb9622ab62398573326
Rafael <rafaeldeps15@gmail.com>
2026-04-29 14:33:29 -0300

    feat: add tile size definition utility based on available RAM; update example script to use dynamic tile size

--- examples/exemple_pansharpening_tiles.py
+++ examples/exemple_pansharpening_tiles.py
@@ -1,26 +1,25 @@
 import os
 import sys
-
-# Força o Python a enxergar a pasta raiz (sigma) como o diretório principal absoluto
 pasta_raiz = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, pasta_raiz)
 
 from utils.pansharpening_tiles import processar_pansharpening_tiles
+from utils.definir_tile import definir_tamanho_tile
 
 if __name__ == "__main__":
-    # Configuração de caminhos e parâmetros
     caminho_pan = "pan.tif"
     caminho_ms = "cor_verdadeira.tif"
     caminho_saida = "pansharpened_output.tif"
-    
-    tamanho_do_tile = 4096 # Reduza para 2048 se faltar memória RAM
+
+    tamanho_do_tile = definir_tamanho_tile()
+
     pasta_temporaria = "./tiles_temp"
 
-    # Validação de segurança
     if not os.path.exists(caminho_pan) or not os.path.exists(caminho_ms):
         raise FileNotFoundError(f"Os arquivos '{caminho_pan}' ou '{caminho_ms}' não foram encontrados na raiz.")
 
-    # Execução silenciosa
+
+
     processar_pansharpening_tiles(
         caminho_pan=caminho_pan,
         caminho_ms=caminho_ms,
