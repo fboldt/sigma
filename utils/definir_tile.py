204aa93f0738ae2d25eba356f66bcaa7eeee9814
RafaelDeps <rafaeldeps15@gmail.com>
2026-05-01 17:08:13 -0300

    feat: refactor tile size calculation to use dynamic sizing based on available RAM

--- utils/definir_tile.py
+++ utils/definir_tile.py
@@ -1,21 +1,10 @@
 import psutil
 
+def calcular_tamanho_tile(ram_gb: float) -> int:
+    multiplicador = max(1, min(16, (ram_gb / 2), (ram_gb / 4) + 4))
+    return int(multiplicador) * 1024
+
+
 def definir_tamanho_tile() -> int:
     ram_disponivel_gb = psutil.virtual_memory().available / (1024 ** 3)
-
-    if ram_disponivel_gb >= 48:
-        return 16384
-    elif ram_disponivel_gb >= 32:
-        return 12288
-    elif ram_disponivel_gb >= 24:
-        return 10240
-    elif ram_disponivel_gb >= 16:
-        return 8192
-    elif ram_disponivel_gb >= 12:
-        return 6144
-    elif ram_disponivel_gb >= 8:
-        return 4096
-    elif ram_disponivel_gb >= 4:
-        return 2048
-    else:
-        return 1024
+    return calcular_tamanho_tile(ram_disponivel_gb)
